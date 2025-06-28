abe9d6acc027ef1514fd3bb0ae007ed2582d5d53
Molly Draven <drazi@duck.com>
2025-06-27 20:15:16 -0400

    feat: Integrate bcrypt for password hashing and enhance AuthLogin class with user management

--- src/AuthLogin.ts
+++ src/AuthLogin.ts
@@ -1,14 +1,26 @@
-import { IncomingMessage, ServerResponse } from "http"
+import { IncomingMessage, ServerResponse } from "node:http"
 import { DatabaseSync } from "node:sqlite"
+import { compare, hashSync } from "bcrypt"
 import { log } from "../server"
+
+const initialUsers: { customerId: number, username: string, password: string }[] = [
+    { customerId: 5551212, username: "admin", password: "admin" }
+]
 
 export class AuthLogin {
     protected db: DatabaseSync
 
     constructor() {
-        this.db = new DatabaseSync("authUsers.db")
+        this.db = new DatabaseSync("authUsers.db", {
+
+        })
         try {
-            this.db.exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, passwordHash TEXT NOT NULL)')
+            this.db.exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, customerId INTEGER UNIQUE ON CONFLICT IGNORE NOT NULL, username TEXT UNIQUE ON CONFLICT IGNORE NOT NULL, passwordHash TEXT NOT NULL)')
+            for (const user of initialUsers) {
+                const hashedPassword = hashSync(user.password, 10)
+                const sql = `INSERT INTO users (customerId, username, passwordHash) VALUES ('${user.customerId}', '${user.username}', '${hashedPassword}')`
+                this.db.exec(sql)
+            }
         } catch (error: unknown) {
             log.error(`Unable to create initial users table: ${(error as Error).message}`)
         }
@@ -17,13 +29,22 @@ export class AuthLogin {
 
 
 
-    handleRequest() {
-        this.db.exec(`Select 1;`)
+    handleRequest(username: string): { customerId: number, username: string, passwordHash: string } | null {
+        const query = this.db.prepare('SELECT customerId, username, passwordHash from users where username = ? LIMIT 1')
+        const record = query.get(username)
+
+        return record ? {
+            customerId: record['customerId'] as number,
+            username: record['username'] as string,
+            passwordHash: record['passwordHash'] as string,
+        } : null
+
+
 
     }
 }
 
-export function handleAuthLoginRoute(request: IncomingMessage, response: ServerResponse) {
+export async function handleAuthLoginRoute(request: IncomingMessage, response: ServerResponse) {
 
     const authLogger = log.child({
         function: handleAuthLoginRoute
@@ -39,7 +60,7 @@ export function handleAuthLoginRoute(request: IncomingMessage, response: ServerR
 
     const username = url.searchParams.get('username')
     const password = url.searchParams.get('password')
-    if ( typeof username === "undefined" || typeof password === "undefined") {
+    if (typeof username === "undefined" || typeof password === "undefined") {
         authLogger.info(`username: ${username}, password: ${password}`)
         response.statusCode = 404;
         response.end('Not Found')
@@ -48,6 +69,25 @@ export function handleAuthLoginRoute(request: IncomingMessage, response: ServerR
 
     const authLogin = new AuthLogin()
     authLogger.info(`username: ${username}, password: ${password}`)
+
+    // I'm fairly sure user and password can't be undefined at this point, but I should confirm,
+    const record = authLogin.handleRequest(username!)
+
+    if (record === null) {
+        log.error(`Unable to locate records for user ${username}`)
+        response.statusCode = 404
+        response.end('not found')
+        return
+    }
+
+    const passwordMatches = await compare(password!, record.passwordHash)
+    if (passwordMatches) {
+        response.statusCode = 200
+        response.end('Auth')
+        return
+    }
+
+
     response.statusCode = 404
     response.end('not found')
 }
